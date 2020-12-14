849f56b1d9137f92140515be22cca95b423e918a
David GABISON <david.gabison@outlook.com>
2020-12-14 13:32:59 +0100

    feat: type parsed command

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const childProcess = require('child_process');
 const yarnPath = require.resolve('yarn/bin/yarn.js');
 
 function execYarn(args) {
+  console.info(yarnPath, ...args);
   childProcess.fork(yarnPath, args);
 }
 
